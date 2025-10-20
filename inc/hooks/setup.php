d0d31368e299acc63cbf63817f54472f335bca21
Nonna Volodina <volodina.nonna@gmail.com>
2025-10-20 13:35:46 -0400

    Update setup.php to exclude gutenberg editor

--- inc/hooks/setup.php
+++ inc/hooks/setup.php
@@ -14,3 +14,6 @@ add_action('after_setup_theme', function () {
 		'script'
 	]);
 });
+
+add_filter( 'use_block_editor_for_post', '__return_false', 10 );
+add_filter( 'use_block_editor_for_post_type', '__return_false', 10 );
