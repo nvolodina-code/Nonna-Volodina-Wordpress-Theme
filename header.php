9c733beee80c0809370c34373204157c83247d7f
Nonna Volodina <nvolodina@marvelandsnap.com>
2026-05-05 13:57:30 -0400

    Removed skip to content link

--- header.php
+++ header.php
@@ -1,14 +1,14 @@
-<?php if ( ! defined('ABSPATH') ) exit; ?>
+<?php if ( ! defined( 'ABSPATH' ) ) {
+	exit;} ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 	<?php wp_body_open(); ?>
-	<a class="u-skip-link" href="#main-content"><?php esc_html_e('Skip to content', 'nonna-volodina-wordpress-theme'); ?></a>
 
 	<header class="site-header"></header>
 
